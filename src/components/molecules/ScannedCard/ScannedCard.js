70a7c9136b5f9c4bddfb21e90e55c4dcbefc181c
ZHAWluci <80687024+ZHAWluci@users.noreply.github.com>
2021-10-01 16:56:43 +0200

    Added Patterns and the active scanned card is now passed to the MetricsContainer from the ArComponent.

---
+++ src/components/molecules/ScannedCard/ScannedCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ScannedCard = (props) => {
+    return (
+        <div>
+            <button>{props.cardName}</button>
+        </div>
+    )
+}
+export default ScannedCard
--- src/components/pages/MetricsContainer/MetricsContainer.js
+++ src/components/pages/MetricsContainer/MetricsContainer.js
@@ -1,20 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import AddMetricForm from '../../organisms/AddMetricForm/AddMetricForm'
 import AddedMetricsList from '../../organisms/AddedMetricsList/AddedMetricsList'
 import ChangeMetric from '../../organisms/ChangeMetric/ChangeMetric'
 import './MetricsContainer.scss'
 
-const MetricsContainer = () => {
+const MetricsContainer = (props) => {
     // ------------------------------------------------------------------------
     // States
     // ------------------------------------------------------------------------
 
     const [showAddMetricForm, setShowAddMetricForm] = useState(true)
     const [showChangeMetric, setShowChangeMetric] = useState(false)
+    const [showScannedCard, setShowScannedCard] = useState(false)
+
     const [currentMetricTitle, setCurrentMetricTitle] = useState("")
     const [currentMetricScore, setCurrentMetricScore] = useState(0)
     
+    // ------------------------------------------------------------------------
+    // Handle Active card
+    // ------------------------------------------------------------------------ 
+    useEffect(()=>{
+        setShowScannedCard(true)
+    },[props.activeCard])
+
     // ------------------------------------------------------------------------
     // Handle Show Metric Form. The closeAddMetircFormHandler is passed to the AddMetirc Form
     // ------------------------------------------------------------------------
@@ -45,6 +54,7 @@ const MetricsContainer = () => {
 
     return (
         <div className="metric-container">
+            {showScannedCard}
             {showChangeMetric && <ChangeMetric currentMetricTitle={currentMetricTitle} initialMetricScore={currentMetricScore} onClose={closeClickHandler}/>}
             {showAddMetricForm && <AddMetricForm onClose={closeAddMetircFormHandler}/>}
             <AddedMetricsList showAddMetricForm={showAddMetricForm} onClickMetric={onClickMetric} onClickNew={onClcikNewHandler}/>
